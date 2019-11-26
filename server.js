e5af66fd3f2ffbda1284ca2fd2851f90d95f3709
Harold Tungala <christianlugod05@gmail.com>
2019-11-27 07:44:28 +0800

    Production Build Fix Finally

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use("/api/options/", require("./routes/options"));
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 
-    app.get("*", (res, req) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
